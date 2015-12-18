de8493b2b86ea1bb64831ecd76d10d9ed00433c3
Kinice <szp93@126.com>
2015-12-18 17:13:14 +0800

    .

--- views/js/header.js
+++ views/js/header.js
@@ -1,62 +1,45 @@
 $(function(){
-	$('.menu').click(function() {
-		/* Act on the event */
-        var randNum = Math.random();
-        if(randNum >= 0 && randNum <0.5){
-            menuSlide();
-        }else{
-            menuFade();
-        }
-	});
-    $('.home').click(function(){
-        backOne();
-    });
-    $(document).scroll(function(){
-        if(getScrollTop() <= 200){
-            $('.back-to-top').fadeOut();
-        }else if(getScrollTop() >= 200){
-            //Back To Top
-            $('.back-to-top').fadeIn();
-        }
-    });
-    $('.back-to-top').click(function(){
-        $('html,body').animate({
-            scrollTop: 0
-        },500);
-    });
-    $('.pin').click(function() {
-        /* Act on the event */
-        var $pin = $(this);
-        var $hd = $('.g-hd');
-        if($hd.css('position')=='absolute'){
-            dispFixed($hd);
-            $pin.css({
-                'transform':'rotate(-90deg)',
-                'color':'#fff'
-            });
-        }else{
-            dispAbsolute($hd);
-            $pin.css({
-                'transform':'rotate(0deg)',
-                'color':'#f26651'
-            });
-        }
-    });
+
 });
-function menuSlide(){
-    if($('.mobile-menu').css('display') == 'block'){
-        $('.mobile-menu').slideUp(150);
-    }else{
-        $('.mobile-menu').slideDown(150);
+$(document).scroll(function(){
+    if(getScrollTop() <= 200){
+        $('.back-to-top').fadeOut();
+    }else if(getScrollTop() >= 200){
+        //Back To Top
+        $('.back-to-top').fadeIn();
     }
-}
-function menuFade(){
-    if($('.mobile-menu').css('display') == 'block'){
-        $('.mobile-menu').fadeOut(150);
+});
+$('.back-to-top').click(function(){
+    $('html,body').animate({
+        scrollTop: 0
+    },500);
+});
+$('.pin').click(function() {
+    var $pin = $(this);
+    var $hd = $('.g-hd');
+    if($hd.css('position')=='absolute'){
+        dispFixed($hd);
+        $pin.css({
+            'transform':'rotate(-90deg)',
+            'color':'#fff'
+        });
     }else{
-        $('.mobile-menu').fadeIn(150);
+        dispAbsolute($hd);
+        $pin.css({
+            'transform':'rotate(0deg)',
+            'color':'#f26651'
+        });
     }
-}
+});
+$('.pic img').click(function() {
+    if($(this).attr('alt')=='s'){
+        $(this).attr('alt','b');
+        $(this).css('width','100%');
+    }else{
+        $(this).attr('alt','s');
+        $(this).css('width','200');
+    }
+});
 function getScrollTop(){
     return $(document).scrollTop();
 }
